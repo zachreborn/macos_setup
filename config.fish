1cb60e9d99aacebcc8a16bea90a19d227ddae412
Zachary Hill <zhill@thinkstack.co>
2022-02-14 17:02:37 -0500

    added config.fish

---
+++ config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+source (pyenv init - | psub)
+oh-my-posh --init --shell fish --config ~/.poshthemes/octo-theme.json | source
