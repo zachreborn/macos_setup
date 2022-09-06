9e28ebf366983a963a207b82f4e6869bdd56a685
Zachary Hill <zhill@thinkstack.co>
2022-09-06 16:35:17 -0500

    updated config.fish with the necessary pyenv commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,12 @@ status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
 source (pyenv init - | psub)
 oh-my-posh --init --shell fish --config ~/.poshthemes/octo-theme.omp.json | source
+
+
+set PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/shims $PATH
+set -x PATH $PYENV_ROOT/bin $PATH
+
+if command -v pyenv 1>/dev/null 2>&1
+    pyenv init - | source
+end
